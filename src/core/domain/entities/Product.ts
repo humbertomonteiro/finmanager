7408cef0c717c0ee2f9e8fa83f1533f12f2a7af7
Humberto Monteiro <humbertoaraujox@icloud.com>
2025-09-01 23:00:06 -0300

    add entidades Production e Transaction

---
+++ src/core/domain/entities/Product.ts
@@ -0,0 +1,86 @@
+export interface ProductProps {
+  id?: string;
+  name: string;
+  costPrice: number;
+  salePrice: number;
+  lastSalePrice?: number;
+  supplier: string | null;
+  description?: string;
+  quantity: number;
+  createdAt?: Date;
+}
+
+export class Product {
+  private readonly props: ProductProps;
+
+  constructor(props: ProductProps) {
+    this.props = {
+      ...props,
+      supplier: props.supplier || null,
+      createdAt: props.createdAt || new Date(),
+    };
+
+    this.validate();
+  }
+
+  private validate() {
+    if (this.name.length <= 1) {
+      throw new Error("Name must be more than 2 characters.");
+    }
+
+    if (this.costPrice <= 0) {
+      throw new Error("Cost price must be greater than 0.");
+    }
+
+    if (this.salePrice <= 0) {
+      throw new Error("Sale price must be greater than 0.");
+    }
+
+    if (this.salePrice < this.costPrice) {
+      throw new Error("Sale price cannot be less than cost price.");
+    }
+  }
+
+  public updateQuantity(amount: number) {
+    if (amount < 0) throw new Error("Quantity cannot be negative.");
+    this.props.quantity = amount;
+  }
+
+  public changeSalePrice(newPrice: number) {
+    if (newPrice <= 0) throw new Error("Sale price must be greater than 0.");
+    this.props.lastSalePrice = this.props.salePrice;
+    this.props.salePrice = newPrice;
+  }
+
+  get id() {
+    return this.props.id;
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get costPrice() {
+    return this.props.costPrice;
+  }
+
+  get salePrice() {
+    return this.props.salePrice;
+  }
+
+  get lastSalePrice() {
+    return this.props.lastSalePrice;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  public toDTO(): ProductProps {
+    return this.props;
+  }
+}
---
+++ src/core/domain/entities/Transaction.ts
@@ -0,0 +1,66 @@
+export type TransactionType = "sale" | "purchase" | "aporte";
+
+export interface TransactionItem {
+  productId: string;
+  quantity: number;
+  unitPrice: number;
+}
+
+export interface TransactionProps {
+  id?: string;
+  type: TransactionType;
+  description?: string;
+  value: number;
+  date: Date;
+  items: TransactionItem[];
+  discount?: number;
+}
+
+export class Transaction {
+  private readonly props: TransactionProps;
+
+  constructor(props: TransactionProps) {
+    this.props = {
+      ...props,
+      date: props.date || new Date(),
+    };
+
+    this.validate();
+  }
+
+  private validate() {
+    if (this.props.value <= 0) {
+      throw new Error("Value must be greater than 0");
+    }
+
+    if (!["sale", "purchase", "aporte"].includes(this.props.type)) {
+      throw new Error("Invalid transaction type. ");
+    }
+  }
+
+  get id() {
+    return this.props.id;
+  }
+  get type() {
+    return this.props.type;
+  }
+  get value() {
+    return this.props.value;
+  }
+  get description() {
+    return this.props.description;
+  }
+  get date() {
+    return this.props.date;
+  }
+  get items() {
+    return this.props.items || [];
+  }
+  get discount() {
+    return this.props.discount;
+  }
+
+  public toDTO(): TransactionProps {
+    return this.props;
+  }
+}
