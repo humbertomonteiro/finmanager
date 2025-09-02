29c221d3e83d8db043cc04d945c623cd46d6e727
Humberto Monteiro <humbertoaraujox@icloud.com>
2025-09-01 22:57:51 -0300

    add RoutesApp com rota / para Dashboard

---
+++ src/routes/RoutesApp.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+import Dashboard from "../pages/Dashboard";
+
+export default function RoutesApp() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
+  );
+}
